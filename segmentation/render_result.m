1c3ddff22a465641f4b39f2bd0ee6afa63835e9e
chenly <chenly19940617@live.cn>
2018-01-02 18:48:27 +0800

    development 2018.01.02

--- segmentation/render_result.m
+++ segmentation/render_result.m
@@ -3,11 +3,11 @@ function [colors] = render_result(labels_result)
 colors = zeros(length(labels_result), 3);
 for i = 1 : length(labels_result)
     if labels_result(i) == 0 % skin
-        colors(i, :) = [255, 255, 0];
+        colors(i, :) = [200, 86, 0];
     elseif labels_result(i) == 1 % t-shirt
-        colors(i, :) = [0, 0, 255];
+        colors(i, :) = [25, 114, 175];
     else % pants
-        colors(i, :) = [255, 0, 0];    
+        colors(i, :) = [29, 139, 58];    
     end
 end
 
