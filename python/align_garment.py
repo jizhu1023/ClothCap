b75e4cc1ed17d53479b5eef6fc0d620dec49c2c3
chenly <chenly19940617@live.cn>
2018-03-02 15:15:00 +0800

    add optimization options

--- python/align_garment.py
+++ python/align_garment.py
@@ -24,11 +24,13 @@ def main():
     mesh_smpl_temp.vertices[garment_smpl.vertices_ind - 1, :] = L
     mesh_smpl_temp.normals = calculation.cal_normals(mesh_smpl_temp.faces, mesh_smpl_temp.vertices)
 
+    # optimization
     param = np.hstack((np.reshape(theta, [1, -1]), np.reshape(L, [1, -1])))
     args = (mesh_scan, mesh_smpl, garment_scan,
             garment_smpl, smpl_param, smpl_model, is_first)
 
-    param_opt = minimize(fun=energy_garments.energy_garments, x0=param, args=args)
+    param_opt = minimize(fun=energy_garments.energy_garments, x0=param,
+                         args=args, method='BFGS', options={'disp': True})
 
     energy = energy_garments.energy_garments(
         param, mesh_scan, mesh_smpl, garment_scan,
